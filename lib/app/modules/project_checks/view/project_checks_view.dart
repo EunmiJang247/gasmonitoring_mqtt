8585abbb753c8922937c728ea1beae555afd7499
JEM <valueyou247@naver.com>
2025-04-30 18:46:45 +0900

    성능점검표-border-검정색으로

--- lib/app/modules/project_checks/view/project_checks_view.dart
+++ lib/app/modules/project_checks/view/project_checks_view.dart
@@ -26,14 +26,12 @@ class _CheckListState extends State<CheckList> {
       child: Column(
         children: [
           Expanded(
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-              color: Colors.white,
-              width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ProjectChecksInfo(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 16, top: 16),
+                  child: ProjectChecksInfo(
                       inspectorName: curProject?.site_check_form?.inspectorName,
                       inspectionDate:
                           curProject?.site_check_form?.inspectionDate,
@@ -41,52 +39,49 @@ class _CheckListState extends State<CheckList> {
                           controller.inspectorNameController,
                       inspectorNameFocus: controller.inspectorNameFocus,
                       onDateChange: controller.onDateChange),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  InspectionCateDropdown(
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 16, right: 16, bottom: 16, top: 4),
+                  child: InspectionCateDropdown(
                     onChanged: controller.takePictureAndSet,
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    height: 2,
-                    decoration: BoxDecoration(
-                      color: Colors.white, // 배경색 추가 (그림자 보이게)
-                      border: Border(
-                        bottom: BorderSide(
-                            color: const Color.fromARGB(31, 77, 77, 77),
-                            width: 1),
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.1),
-                          spreadRadius: 0,
-                          blurRadius: 6,
-                          offset: Offset(0, 4), // 아래로만 그림자
-                        ),
-                      ],
+                ),
+                Container(
+                  height: 2,
+                  decoration: BoxDecoration(
+                    color: Colors.white, // 배경색 추가 (그림자 보이게)
+                    border: Border(
+                      bottom: BorderSide(color: Colors.black87, width: 1),
                     ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.1),
+                        spreadRadius: 0,
+                        blurRadius: 6,
+                        offset: Offset(0, 4), // 아래로만 그림자
+                      ),
+                    ],
                   ),
-                  Expanded(
-                    child: Obx(() => ListView.builder(
-                          padding: EdgeInsets.only(top: 16.h),
-                          itemCount: controller.curProject.value
-                                  ?.site_check_form?.data.length ??
-                              0,
-                          itemBuilder: (context, index) {
-                            final item = controller
-                                .curProject.value?.site_check_form?.data[index];
-                            return Padding(
-                              padding: EdgeInsets.only(bottom: 16.h),
-                              child: ProjectChecksResultCards(data: item),
-                            );
-                          },
-                        )),
-                  ),
-                ],
-              ),
+                ),
+                Expanded(
+                  child: Obx(() => ListView.builder(
+                        padding: EdgeInsets.only(top: 16.h),
+                        itemCount: controller.curProject.value?.site_check_form
+                                ?.data.length ??
+                            0,
+                        itemBuilder: (context, index) {
+                          final item = controller
+                              .curProject.value?.site_check_form?.data[index];
+                          return Padding(
+                            padding: EdgeInsets.only(
+                                bottom: 16.h, left: 16, right: 16),
+                            child: ProjectChecksResultCards(data: item),
+                          );
+                        },
+                      )),
+                ),
+              ],
             ),
           ),
         ],
