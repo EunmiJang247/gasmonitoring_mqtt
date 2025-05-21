8f5d3ecbbce246efb32d06db4518690795a11aaf
JEM <valueyou247@naver.com>
2025-05-21 11:26:35 +0900

    스플레쉬없애기위한mainactivitykt변경경

--- android/app/src/main/kotlin/com/elim/safety_check/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.elim.safety_check
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/jenny/meditation_friend/MainActivity.kt
+++ android/app/src/main/kotlin/com/jenny/meditation_friend/MainActivity.kt
@@ -1,6 +1,24 @@
 package com.jenny.meditation_friend
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setBackgroundDrawableResource(android.R.color.transparent)
+        window.addFlags(
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS or
+            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE or
+            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+        )
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
